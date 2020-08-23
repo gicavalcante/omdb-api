b8598259fff9a986f2ccf441c85a95f130618780
Giovanna Cavalcante <gigabcavalcante@gmail.com>
2020-08-23 16:52:30 -0300

    :wink: refactor to use a single view function

--- vanilla-client/app.js
+++ vanilla-client/app.js
@@ -1,4 +1,5 @@
 
+const app = document.querySelector('#app');
 const form = document.querySelector('form');
 const input = document.querySelector('#searchTerm');
 const resultsSection = document.querySelector('#results');
@@ -6,13 +7,20 @@ const FavoriteSection = document.querySelector('#favorite');
 
 const API_URL='http://www.omdbapi.com/?apikey=45ebdd49&s=';
 
-const state = {
+let state = {
     searchTerm: '' ,
     results:  [],
-    favorite: []
+    favorite: [],
+    error: '',
 };
+ render(state);
 
-input.addEventListener('keyup', () =>{
+ function setState(newStateValues){
+   state = {...state, ...newStateValues};
+   render(state);
+ }
+
+input.addEventListener('keyup', () => {
   state.searchTerm = input.value;
   
 });
@@ -21,12 +29,17 @@ form.addEventListener('submit', formSubmitted);
 
  async function formSubmitted(event) {
   event.preventDefault();
-  const searchTerm = input.value;
   try {
-    state.results = await getResults(state.searchTerm);
-      showResults(results);
+   const results = await getResults(state.searchTerm);
+    setState({
+      results,
+      error:''
+    });
     } catch(error) {
-     showError(error);
+      setState({
+        results: [],
+        error: error.massage
+      });
     }
 }
 
@@ -40,33 +53,15 @@ async function getResults(searchTerm){
    return data.Search;
 }
 
-function showResults (){
-  resultsSection.innerHTML = state.results.reduce((html, movie) => {
-    return html + getMovieTemplate(movie, 4);
-  }, '');
-
-  addButtonListeners();
-}
-
-function addButtonListeners(){
-  const addFavoriteMovie = document.querySelectorAll('.add-favorite');
-  addFavoriteMovie.forEach(button => {
-  button.addEventListener('click', buttonClicked);
-  });
-}
-
 function buttonClicked(event){
         const { id } = event.target.dataset;
         const movie = state.results.find(movie => movie.imdbID === id);
-        state.favorite.push(movie);
-        updateFavoriteSection();
+        setState({
+          favorite: [...state.favorite, movie]
+        })
 };
 
-function updateFavoriteSection(){
-    FavoriteSection.innerHTML = state.favorite.reduce((html, movie) => {
-      return html + getMovieTemplate(movie, 12, false);
-    }, '');
-}
+
 
 function getMovieTemplate(movie, cols, button = true){
     return `
@@ -77,7 +72,7 @@ function getMovieTemplate(movie, cols, button = true){
         <p class="card-text">${movie.Year}</p>
         ${ 
           button ?
-          `<button data-id="${movie.imdbID}" type="button" class="btn btn-danger add-favorite"> Add to Favorite </button>`
+          `<button onclick="buttonClicked(event) "data-id="${movie.imdbID}" type="button" class="btn btn-danger add-favorite"> Add to Favorite </button>`
           : ''
         }  
       </div>
@@ -85,11 +80,31 @@ function getMovieTemplate(movie, cols, button = true){
     `;
 }
 
-function showError(error) {
-  resultsSection.innerHTML = `
-<div class="alert alert-primary col" role="alert">
- ${error.message}
-</div>
-`;
+function render(){
+  app.innerHTML = `
+  <section class="row movies-area">
+  <section class="mt-2 col-9 row" id="results">
+      ${
+        !state.error ?
+        state.results.reduce((html, movie) => {
+        return html + getMovieTemplate(movie, 4);
+      }, '')
+      : 
+      `<div class="alert alert-primary col" role="alert">
+       ${state.error}
+      </div>`
+    }
+  </section>
+  <section class=" ml-2 col-3 row" id="favorite">
+      <h3> My Favorites </h3>
+      <section class="row" id=" ml-2 favorites">
+      ${
+        state.favorite.reduce((html, movie) => {
+        return html + getMovieTemplate(movie, 12, false);
+      }, '')
+    }
+      </section>
+  </section>
+</section>     
+  `
 }
-
